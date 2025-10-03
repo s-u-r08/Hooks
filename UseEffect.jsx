79618004804903e450df71df57004c439c58fac3
s-u-r08 <csurbhi11@gmail.com>
2025-10-03 12:39:24 +0530

    Add files via upload

---
+++ UseEffect.jsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from 'react'
+
+import './App.css'
+
+function UseEffect() {
+const [count,setCount]=useState(0)
+const [name,setName]=useState("John")
+useEffect(()=>{
+  setTimeout(()=>{
+    setCount(count=>count+1);
+  },2000)
+},[count,name])
+  return (
+    <>
+      <h1>I've rendered {count} times!</h1>
+    </>
+  )
+}
+
+export default UseEffect
